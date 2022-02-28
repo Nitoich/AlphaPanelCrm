c511d45cd7f39d2d6dc2fcb3692b90666d01ac11
Nitoich <bcmamka@gmail.com>
2022-02-28 21:57:01 +0300

    Сделал прозрачность хедера задач

--- app.js
+++ app.js
@@ -3,3 +3,7 @@
 const MenuPanels = document.querySelector('div.main-buttons');
 MenuPanels.style.setProperty('--mib-background', 'transparent');
 MenuPanels.style.setProperty('--mib-item-color', '#fff');
+
+// Прозрачность хедера таблицы с задачами
+const TaskTableHeader = document.querySelector('tr.main-grid-row-head');
+TaskTableHeader.style.backgroundColor = 'transparent';
