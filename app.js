0ebdb0aa30edb7eebef286938f4f26c73392be1a
Nitoich <bcmamka@gmail.com>
2022-02-28 21:39:28 +0300

    Create repository and Сделал прозрачность на меню

---
+++ app.js
@@ -0,0 +1,5 @@
+
+// Прозрачность на меню
+const MenuPanels = document.querySelector('div.main-buttons');
+MenuPanels.style.setProperty('--mib-background', 'transparent');
+MenuPanels.style.setProperty('--mib-item-color', '#fff');
