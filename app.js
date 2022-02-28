cc7d1c8362a5e8310881f198a59466c25802cb4c
Nitoich <bcmamka@gmail.com>
2022-02-28 22:27:57 +0300

    Сделал простенькую прозрачность списка задач

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 
+function setTransparentBg(element) {
+    element.style.background = 'transparent';
+    element.style.backgroundColor = 'transparent';
+}
+
+const el = (selector) => { return document.querySelector(selector) }
+
 // Прозрачность на меню
 const MenuPanels = document.querySelector('div.main-buttons');
 MenuPanels.style.setProperty('--mib-background', 'transparent');
 MenuPanels.style.setProperty('--mib-item-color', '#fff');
 
 // Прозрачность хедера таблицы с задачами
-const TaskTableHeader = document.querySelector('tr.main-grid-row-head');
-TaskTableHeader.style.backgroundColor = 'transparent';
+
+setTransparentBg(el('tr.main-grid-row-head'));
+
+setTransparentBg(el('div.tasks-grid-wrapper'));
+
+setTransparentBg(el('table.main-grid-table').children[1]);
+
+setTransparentBg(el('div.main-grid-panel-wrap'));
+
+
+setTransparentBg(el('div.main-grid-control-panel-wrap'));
+
+const TasksList = document.querySelectorAll('td.main-grid-cell');
+TasksList.forEach((Task) => {
+    Task.style.background = 'transparent';
+});
